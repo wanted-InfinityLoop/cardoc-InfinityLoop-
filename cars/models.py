e339cc5ece112b7a742ca2854a5201411f323356
Oderystella <ddott59@gmail.com>
2021-11-24 15:28:23 +0900

    feat: modeling and migrate
    - User, Car, Trim, Tire models 작성
    - makemigration ->  migrate

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    brand    = models.CharField(max_length=16)
+    model    = models.CharField(max_length=16)
+    submodel = models.CharField(max_length=16)
+    year     = models.PositiveSmallIntegerField()
+
+    class Meta:
+        db_table = "cars"
+
+
+class Trim(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    car  = models.ForeignKey(Car, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "trims"
+
+
+class Tire(models.Model):
+    name         = models.CharField(max_length=16)
+    width        = models.PositiveSmallIntegerField()
+    aspect_ratio = models.PositiveSmallIntegerField()
+    wheel_size   = models.PositiveSmallIntegerField()
+    trim = models.ForeignKey(Trim, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = "tires"
+
--- users/models.py
+++ users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    id       = models.CharField(max_length=16, primary_key=True)
+    password = models.CharField(max_length=128)
+
+    class Meta:
+        db_table = "users"
